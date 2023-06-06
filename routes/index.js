b1aeb09dc385c1e5f6e83bc3f7b308cc4a4217c1
iCoder <akoley012@gmail.com>
2023-06-06 14:34:07 +0530

    root redirect to products

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/products", require("./product"));
+router.use("/", (req, res) => {
+    return res.redirect("/products");
+});
 
 module.exports = router;
