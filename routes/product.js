00339aa77a987ced5a27ed62cddf2a6b912756ae
iCoder <akoley012@gmail.com>
2023-06-19 21:47:02 +0530

    sequence update

--- routes/product.js
+++ routes/product.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 //! create product
 router.post("/create", create);
-//! get all products
-router.get("/", getAll);
-//! delete a product
-router.delete("/:id", deleteProduct);
 //! update quantity of product
 router.post("/:id/update_quantity", update);
+//! delete a product
+router.delete("/:id", deleteProduct);
+//! get all products
+router.get("/", getAll);
 
 module.exports = router;
